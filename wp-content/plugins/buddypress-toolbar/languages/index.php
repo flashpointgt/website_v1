ed4e69ba8f96a496587b09077a98516b4fe1eb95
RyanWH <ryan@propercursive.com>
2013-02-22 14:54:27 -0500

    remove buddypress plugins

--- wp-content/plugins/buddypress-toolbar/languages/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Do not put custom translations here. They will be deleted on 'BuddyPress Toolbar' updates!
- *
- * Keep custom 'BuddyPress Toolbar' translations in '/wp-content/languages/buddypress-toolbar/'
- */
