a352840382f5b267786b426e030c74b03036df6a
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-03-28 14:09:55 -0400

    test ie9 modernizer class

--- wp-content/themes/flashpoint/header-home.php
+++ wp-content/themes/flashpoint/header-home.php
@@ -4,6 +4,7 @@
 <!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
 <!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	
 	<head>
