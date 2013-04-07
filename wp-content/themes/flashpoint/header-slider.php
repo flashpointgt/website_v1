b0dcc7e20a94d256789cc62f6338aa940f16acd2
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-04-07 09:52:55 -0400

    add test images to slider

--- wp-content/themes/flashpoint/header-slider.php
+++ wp-content/themes/flashpoint/header-slider.php
@@ -151,21 +151,21 @@
 	            </ol>
 	            <div class="carousel-inner">
 	              <div class="item">
-	                <img src="assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
+	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
 	                <div class="carousel-caption">
 	                  <h4>First Thumbnail label</h4>
 	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 	                </div>
 	              </div>
 	              <div class="item">
-	                <img src="assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
+	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
 	                <div class="carousel-caption">
 	                  <h4>Second Thumbnail label</h4>
 	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 	                </div>
 	              </div>
 	              <div class="item active">
-	                <img src="assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
+	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
 	                <div class="carousel-caption">
 	                  <h4>Third Thumbnail label</h4>
 	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
