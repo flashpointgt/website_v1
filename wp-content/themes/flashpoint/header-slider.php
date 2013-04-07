5a9a2944d3002d176f2041be1512ce13ea5f3971
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-04-07 12:00:36 -0400

    remove dummy captions from homepage slider

--- wp-content/themes/flashpoint/header-slider.php
+++ wp-content/themes/flashpoint/header-slider.php
@@ -152,24 +152,16 @@
 	            <div class="carousel-inner">
 	              <div class="item">
 	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
-	                <div class="carousel-caption">
+	                <!-- <div class="carousel-caption">
 	                  <h4>First Thumbnail label</h4>
 	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-	                </div>
+	                </div> -->
 	              </div>
 	              <div class="item">
 	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
-	                <div class="carousel-caption">
-	                  <h4>Second Thumbnail label</h4>
-	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-	                </div>
 	              </div>
 	              <div class="item active">
 	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
-	                <div class="carousel-caption">
-	                  <h4>Third Thumbnail label</h4>
-	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-	                </div>
 	              </div>
 	            </div>
 	            <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
