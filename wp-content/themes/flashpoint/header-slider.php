4fc3c5c11e4570242d7ce6b8603c1ee10d5a7237
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-04-07 12:11:49 -0400

    update dummy image

--- wp-content/themes/flashpoint/header-slider.php
+++ wp-content/themes/flashpoint/header-slider.php
@@ -151,17 +151,17 @@
 	            </ol>
 	            <div class="carousel-inner">
 	              <div class="item">
-	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
+	                <img src="http://dummyimage.com/980x400/000/fff" alt="">
 	                <!-- <div class="carousel-caption">
 	                  <h4>First Thumbnail label</h4>
 	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 	                </div> -->
 	              </div>
 	              <div class="item">
-	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
+	                <img src="http://dummyimage.com/980x400/000/fff" alt="">
 	              </div>
 	              <div class="item active">
-	                <img src="http://dummyimage.com/600x400/000/fff" alt="">
+	                <img src="http://dummyimage.com/980x400/000/fff" alt="">
 	              </div>
 	            </div>
 	            <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
