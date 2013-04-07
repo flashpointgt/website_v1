200ed502eccc6da0653bd05d5eaaf2c3de206d75
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-04-07 12:18:05 -0400

    move slider

--- wp-content/themes/flashpoint/header-slider.php
+++ wp-content/themes/flashpoint/header-slider.php
@@ -143,28 +143,4 @@
 		<div class="container-fluid logo">
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="" /></a>
 		</div>
-			<div id="myCarousel" class="carousel slide">
-	            <ol class="carousel-indicators">
-	              <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-	              <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-	              <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
-	            </ol>
-	            <div class="carousel-inner">
-	              <div class="item">
-	                <img src="http://dummyimage.com/980x400/000/fff" alt="">
-	                <!-- <div class="carousel-caption">
-	                  <h4>First Thumbnail label</h4>
-	                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-	                </div> -->
-	              </div>
-	              <div class="item">
-	                <img src="http://dummyimage.com/980x400/000/fff" alt="">
-	              </div>
-	              <div class="item active">
-	                <img src="http://dummyimage.com/980x400/000/fff" alt="">
-	              </div>
-	            </div>
-	            <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-	            <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
-        	</div>
 		<div class="container-fluid main">		
--- wp-content/themes/flashpoint/page-slider.php
+++ wp-content/themes/flashpoint/page-slider.php
@@ -7,7 +7,30 @@ Template Name: Homepage w/ slider
 <?php get_header('slider'); ?>
 			
 			<div id="content" class="clearfix row-fluid">
-			
+				<div id="myCarousel" class="carousel slide">
+		        	<ol class="carousel-indicators">
+		            	<li data-target="#myCarousel" data-slide-to="0" class=""></li>
+		            	<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+		            	<li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+		            </ol>
+		            <div class="carousel-inner">
+		            	<div class="item">
+		                	<img src="http://dummyimage.com/980x400/000/fff" alt="">
+		                	<!-- <div class="carousel-caption">
+		                  	<h4>First Thumbnail label</h4>
+		                  	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                	</div> -->
+		              	</div>
+		              	<div class="item">
+		                	<img src="http://dummyimage.com/980x400/000/fff" alt="">
+		              	</div>
+		              	<div class="item active">
+		                	<img src="http://dummyimage.com/980x400/000/fff" alt="">
+		              	</div>
+		            </div>
+		            <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+		            <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+        		</div>
 				<div id="main" class="span12 clearfix" role="main">
 
 					<!-- Remove built-in slider for custom implementation
